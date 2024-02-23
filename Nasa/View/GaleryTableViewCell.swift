05d0d3f0c0fee6b6d3d2aa6bc344beda83fb2cc4
MarcosMello <marcosvinicius-vm@outlook.com>
2024-02-23 13:39:18 -0300

    Feat: Adds changes in the Main View and a Custom Table View Cell

--- Nasa/View/GaleryTableViewCell.swift
+++ Nasa/View/GaleryTableViewCell.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class GaleryTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var marsRoverImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
