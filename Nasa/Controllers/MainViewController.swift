0bfcf5550b7086a06ef7831e7c21bd05573e029f
MarcosMello <marcosvinicius-vm@outlook.com>
2024-02-23 13:40:19 -0300

    Feat: Removes IBAction on Button to perform Segue

--- Nasa/Controllers/MainViewController.swift
+++ Nasa/Controllers/MainViewController.swift
@@ -5,9 +5,5 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    @IBAction func APODButtonTapped(_ sender: UIButton) {
-        performSegue(withIdentifier: "APODSegue", sender: self)
-    }
 }
 
