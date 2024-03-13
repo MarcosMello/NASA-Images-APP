86a3332e62bae08cb533397b5f3398ef64ab36d8
MarcosMello <marcosvinicius-vm@outlook.com>
2024-03-12 22:14:20 -0300

    Refactor: Chnaged from delegate to escaping clojures

--- Nasa/Model/NetworkingManager.swift
+++ Nasa/Model/NetworkingManager.swift
@@ -8,23 +8,15 @@ enum NetworkingManagerError: Error {
     case responseCouldNotBeParsed
 }
 
-protocol NetworkingManagerDelegate {
-    func onSuccess<T>(_ networkingMaganer: NetworkingManager<T>, with decodableModel: Decodable)
-    func onFail(with error: Error)
-}
-
 struct NetworkingManager<T: Decodable> {
     let baseURL: String?
     
-    var delegate: NetworkingManagerDelegate?
-    
-    func prepareURL(endpoint: String, apiKey: String, queryParameters: [String]? = nil) -> String? {
+    func prepareURL(endpoint: String, queryParameters: [String]? = nil) -> String? {
         guard var unwrappedURL = self.baseURL else {
-            delegate?.onFail(with: NetworkingManagerError.baseUrlIsNIL)
             return nil
         }
         
-        unwrappedURL += "\(endpoint)?api_key=\(apiKey)"
+        unwrappedURL += "\(endpoint)?api_key=\(Constants.nasaApiKey)"
         
         if let unwrappedQueryParameters = queryParameters {
             unwrappedURL += "&" + unwrappedQueryParameters.joined(separator: "&")
@@ -33,42 +25,44 @@ struct NetworkingManager<T: Decodable> {
         return unwrappedURL
     }
     
-    func getTask(with urlString: String) {
+    func getTask(endpoint: String, queryParameters: [String]? = nil, completion: @escaping (Result<T, NetworkingManagerError>) -> Void) {
+        guard let urlString = prepareURL(endpoint: endpoint, queryParameters: queryParameters) else {
+            completion(.failure(NetworkingManagerError.baseUrlIsNIL))
+            return
+        }
+        
         guard let url = URL(string: urlString) else {
-            delegate?.onFail(with: NetworkingManagerError.urlCouldBeFormed)
+            completion(.failure(NetworkingManagerError.urlCouldBeFormed))
             return
         }
         
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { (data, response, error) in
-            if let unwrappedError = error {
-                delegate?.onFail(with: NetworkingManagerError.requestError(error: unwrappedError))
+            guard error == nil else {
+                completion(.failure(NetworkingManagerError.requestError(error: error ?? NetworkingManagerError.dataIsNIL)))
                 return
             }
             
             guard let unwrappedData = data else {
-                delegate?.onFail(with: NetworkingManagerError.dataIsNIL)
+                completion(.failure(NetworkingManagerError.dataIsNIL))
                 return
             }
             
             guard let apiResponse = self.parseJSON(unwrappedData) else {
-                delegate?.onFail(with: NetworkingManagerError.responseCouldNotBeParsed)
+                completion(.failure(NetworkingManagerError.responseCouldNotBeParsed))
                 return
             }
             
-            delegate?.onSuccess(self, with: apiResponse)
+            completion(.success(apiResponse))
         }
         
         task.resume()
     }
     
-    func parseJSON(_ apiResponse: Data) -> T? {
-        let decoder = JSONDecoder()
-        
+    func parseJSON(_ apiResponse: Data) -> T? {        
         do {
-            return try decoder.decode(T.self, from: apiResponse)
+            return try JSONDecoder().decode(T.self, from: apiResponse)
         } catch {
-            delegate?.onFail(with: error)
             return nil
         }
     }
